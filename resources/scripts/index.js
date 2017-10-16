053939506b8b3f9b3728c697ac35650f4708da1b
Alec Buchanan <alecbuchanan@Alecs-MacBook-Pro.local>
2017-10-16 13:58:34 +0000

    ajax added

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-    $.ajax({url: "../data/names.json", method:"GET", success: function(result){
-        console.log(result);
-    }});
+$(document).ready(function () {
+    // $.ajax({url: "../data/names.json", method:"GET", success: function(result){
+    //     console.log(result);
+    // }});
+    $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+        }
+    });
+
 });
